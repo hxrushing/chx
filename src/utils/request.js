97f54f4b60f14657323a7936405ba80cbc1d4969
hxrushing <202205720406@zjut.edu.cn>
2025-03-29 22:04:46 +0800

    登录 -axios封装处理

---
+++ src/utils/request.js
@@ -0,0 +1,35 @@
+//axio的封装处理
+import axios from "axios";
+//根域名配置
+//超时时间
+//请求拦截器/响应拦截器
+
+const request = axios.create({
+    baseURL: 'http://geek.itheima.net/v1_0',
+    timeout: 5000
+})
+
+request.interceptors.request.use((config) => {
+    const token = getToken()
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  }, (error) => {
+    return Promise.reject(error)
+  })
+  
+  // 添加响应拦截器
+  request.interceptors.response.use((response) => {
+    return response.data
+  }, (error) => {
+    console.dir(error)
+    if (error.response.status === 401) {
+      removeToken()
+      router.navigate('/login')
+      window.location.reload()
+    }
+    return Promise.reject(error)
+  })
+
+export { request }
