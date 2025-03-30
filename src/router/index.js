be0dc48d5c6ea1baf638b062a13caab1d03ac561
hxrushing <202205720406@zjut.edu.cn>
2025-03-30 21:55:19 +0800

    token控制路由组件

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "@/pages/layout";
 import Login from "@/pages/login";
+import { AuthRoute } from "@/components/AuthRoute";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <Layout />
+        element: <AuthRoute><Layout /></AuthRoute>
     },
     {
         path: "/Login",
