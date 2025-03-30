5501b1fb695970a17b36536e0441b96ea4add7b6
hxrushing <202205720406@zjut.edu.cn>
2025-03-30 22:25:05 +0800

    根据路由路径高亮菜单

--- src/pages/layout/index.js
+++ src/pages/layout/index.js
@@ -6,7 +6,7 @@ import {
   LogoutOutlined,
 } from '@ant-design/icons'
 import './index.scss'
-import { Outlet, useNavigate } from 'react-router-dom'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
 
 const { Header, Sider } = Layout
 
@@ -34,6 +34,8 @@ const GeekLayout = () => {
     const path = route.key
     navigate(path)
   }
+  const location = useLocation()
+  const selectedkey = location.pathname
   return (
     <Layout>
       <Header className="header">
@@ -52,7 +54,7 @@ const GeekLayout = () => {
           <Menu
             mode="inline"
             theme="dark"
-            defaultSelectedKeys={['1']}
+            defaultSelectedKeys={selectedkey}
             onClick={onMenuClick}
             items={items}
             style={{ height: '100%', borderRight: 0 }}></Menu>
