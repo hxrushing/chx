e92d60b15eb02846b9e2c0c41a0a57949fcfc7c4
hxrushing <202205720406@zjut.edu.cn>
2025-04-05 19:45:28 +0800

    基础文章发布

--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -14,10 +14,37 @@ import {
   import './index.scss'
   import ReactQuill from 'react-quill'
   import 'react-quill/dist/quill.snow.css'
-  
+  import { createArticleAPI, getChannelAPI } from '@/apis/article'
+  import { useEffect, useState } from'react'
   const { Option } = Select
   
   const Publish = () => {
+    //获取频道列表
+    const [channelList, setChannelList] = useState([])
+    useEffect(() => {
+      //封装函数，在函数体内调用接口
+      const getChannelListAPI = async () => {
+        const res = await getChannelAPI()
+        setChannelList(res.data.channels)
+      }
+      getChannelListAPI()
+    }, [])
+    //表单提交
+    const onFinish = (formValue) => {
+      //按照接口文档的格式处理收集到的表单数据
+      const { title, content, channel_id } = formValue
+      const reqData = {
+        title,
+        content,
+        cover: {
+          images: [],
+          type: 0
+        },
+        channel_id
+      }
+      //调用接口提交
+      createArticleAPI(reqData)
+    }
     return (
       <div className="publish">
         <Card
@@ -33,6 +60,7 @@ import {
             labelCol={{ span: 4 }}
             wrapperCol={{ span: 16 }}
             initialValues={{ type: 1 }}
+            onFinish={onFinish}
           >
             <Form.Item
               label="标题"
@@ -47,7 +75,7 @@ import {
               rules={[{ required: true, message: '请选择文章频道' }]}
             >
               <Select placeholder="请选择文章频道" style={{ width: 400 }}>
-                <Option value={0}>推荐</Option>
+                {channelList.map(item => <Option key={item.id} value={item.id}>{item.name}</Option>)}
               </Select>
             </Form.Item>
             <Form.Item
