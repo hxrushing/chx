f34e163214180a469ff0b775253129c05ef4e3c6
hxrushing <202205720406@zjut.edu.cn>
2025-04-06 18:34:21 +0800

    实现切换封面类型

--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -45,6 +45,17 @@ import {
       //调用接口提交
       createArticleAPI(reqData)
     }
+    //上传回调
+    const [imageList, setImageList] = useState([])
+    const onChange = (value) => {
+      setImageList(value.fileList)
+    }
+    //切换图片封面类型
+    const [imageType, setImageType] = useState(0)
+    const onTypeChange = (e) => {
+      setImageType(e.target.value)
+    }
+    
     return (
       <div className="publish">
         <Card
@@ -59,7 +70,7 @@ import {
           <Form
             labelCol={{ span: 4 }}
             wrapperCol={{ span: 16 }}
-            initialValues={{ type: 1 }}
+            initialValues={{ type: 0 }}
             onFinish={onFinish}
           >
             <Form.Item
@@ -80,20 +91,23 @@ import {
             </Form.Item>
             <Form.Item label="封面">
             <Form.Item name="type">
-              <Radio.Group>
+              <Radio.Group onChange={onTypeChange}>
                 <Radio value={1}>单图</Radio>
                 <Radio value={3}>三图</Radio>
                 <Radio value={0}>无图</Radio>
               </Radio.Group>
             </Form.Item>
-            <Upload
+            {imageType>0 && <Upload
               listType="picture-card"
               showUploadList
+              action={'http://geek.itheima.net/v1_0/upload'}
+              name="image"
+              onChange={onChange}
             >
               <div style={{ marginTop: 8 }}>
                 <PlusOutlined />
               </div>
-            </Upload>
+            </Upload>}
           </Form.Item>
             <Form.Item
               label="内容"
